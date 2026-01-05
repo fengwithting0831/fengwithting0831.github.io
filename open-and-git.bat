248936a97dad6dd0c5f07f6de7ea55e101c89e51
fengwithting0831 <fengwithting0831@gmail.com>
2026-01-05 15:27:52 +0800

    Delete open-and-git.bat

--- open-and-git.bat
+++
@@ -1,9 +0,0 @@
-cd "C:\Users\chbon\OneDrive\Documents\ty11\demo-eleventy-from-scratch-blog"
-git init
-git add .
-git commit -m "20250804 v1"
-git remote add origin https://github.com/fengwithting0831/fengwithting0831.github.io.git
-git checkout -b main
-git push -u origin main --force
-explorer   http://localhost:8080/
-npx @11ty/eleventy --serve
